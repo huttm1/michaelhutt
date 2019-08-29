3943959d4a1fc588f0a0b81c81a93333dd2e0423
huttm1 <52938330+huttm1@users.noreply.github.com>
2019-08-29 14:23:52 -0700

    Add files via upload

--- javascript/myjs.js
+++ javascript/myjs.js
@@ -1,14 +1,10 @@
-function oNav() {
-		
-		if (document.getElementById("mySubmenu").style.display== "block")){
-			document.getElementById("mySubmenu").style.display= "none"
-		}
-		else{
-			document.getElementById("mySubmenu").style.display= "block"
-		}
-}
 
 function closeNav() {
 	
 	document.getElementById("mySubmenu").style.display= "none";
+}
+
+function openNav() {
+	
+	document.getElementById("mySubmenu").style.display= "block";
 }
