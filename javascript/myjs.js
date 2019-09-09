1666046de07d394e81755e3d5d968e0a7c6d20aa
huttm1 <52938330+huttm1@users.noreply.github.com>
2019-09-09 10:39:37 -0700

    submenu revert
    reverted back to droupdown on hover. Added a no-touch class

--- javascript/myjs.js
+++ javascript/myjs.js
@@ -1,3 +1,16 @@
+if (!("ontouchstart" in document.documentElement)) {
+document.documentElement.className += "no-touch";
+}
+function initMap() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+		});
+}
+function codeAddress(){
+	var name = document.getElementById('address');
+	alert("Hello, "+name.value + ".\n It is so nice to meet you!");
+}
 
 function checkNav() {
 	if (document.getElementById("mySubmenu").style.display=="none"){
@@ -10,3 +23,13 @@ function checkNav() {
 		document.getElementById("mySubmenu").style.display= "block";
 	}
 }
+
+function openNav() {
+		document.getElementById("mySubmenu").style.display= "block";	
+}
+
+function closeNav() {
+	
+	document.getElementById("mySubmenu").style.display= "none";
+}
+
